3f5261ab8220e506499ec6ec95788a1196173e0e
Alex G <146039097+axgib@users.noreply.github.com>
2023-10-10 13:31:59 -0700

    Day 3
    Made some progress on P3 - instructors notes helped. Still need to figure out how to get the gain of the TustApprox from the freq

--- MAE144/AJG_C2D_matched.m
+++ MAE144/AJG_C2D_matched.m
@@ -1,19 +1,45 @@
-function [] = AJG_C2D_matched(Ds, omegab)
-% function [] = AJG_C2D_matched(omega, Ds)
-% Convert D(s) to D(z)
-% INPUTS omega bar (freq of interest), D(s)
-% OUTPUTS D(z)
+function [Dz] = AJG_C2D_matched(user_Ds, user_omegab)
+    % function [] = AJG_C2D_matched(omega, Ds)
+    % Convert D(s) to D(z)
+    % INPUTS omega bar (freq of interest), D(s)
+    % OUTPUTS D(z)
 
+    %assume intial conditions
 
-    switch nargin
-        case 0
-            syms z p
-            Ds = RR_tf([ 1 z], [1 p 0])
-            h = 1
-            z = Ds.z
-            p = Ds.p 
+    syms z p
+    Ds = RR_tf([ 1 z], [1 p 0])
+    h = 1
+    omegab = 1
 
-            p_z = exp(p * h)
-            z_z = exp(z * h)
-        Dz = RR_tf(z_z, p_z, 1)
+        switch nargin
+            case 1 %Only Ds is user defined
+                Ds = user_Ds
+
+            case 2 %User defined Ds and OmegaBar
+
+                Ds = user_Ds
+                omegab = user_omegab
+        end
+
+    z = Ds.z
+    p = Ds.p 
+    %map from s to z using z = exp(s*h)
+    p_z = exp(p * h)
+    z_z = exp(z * h)
+    % if there is an infintie zero -> z = -1 
+    z_z(z_z == 0) = -1 %make all of the infinte zeros z = -1
+    % if strickly casual make an infinte zero z = inf
+
+    %build the gain
+    f=2*(1-cos(omegab*h))/(omegab*h*sin(omegab*h))
+    c=2/(f*h)
+    
+    %Pack together - reform the transform fn
+    Dz = RR_tf(z_z, p_z, 1)
 end
+
+%THings to add
+% User defined casualty - If stricly casual then one infinite zero becomes inf
+% user defined omega
+% user defined Ds
+% add the gain --- gain of z matches gain of s at an omegabar
