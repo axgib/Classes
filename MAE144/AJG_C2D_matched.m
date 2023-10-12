0ed44be5ecd9bd45eeb7c3c221f45f7ca057490e
Alex G <146039097+axgib@users.noreply.github.com>
2023-10-12 12:19:47 -0700

    Final Day
    Closer. Added comments to code
    
    Questioning if the gain found in our system is accurate - will inspect after class

--- MAE144/AJG_C2D_matched.m
+++ MAE144/AJG_C2D_matched.m
@@ -1,52 +1,42 @@
 function [Dz] = AJG_C2D_matched(user_Ds, h, user_causal, omega)
-    sympref('FloatingPointOutput', true);
+    %function: AJG_C2D_matched(Ds, h, causal, omega)
+    %INPUTS: Ds is the transfer function that needs to be converted from the s domain to the z domain
+    % h = the time step
+    % causal = Either "semi" or "strict" - if the user defines "strict" causal then ensure that the order of the numerator is 1 less than the order of the denominator
+    % omega = the frequency of interest
+    %OUTPUTS: Dz is the transfer function that Ds maps to in the z domain - it will be in RR_tf format
+    %TEST CASE: The test case is written in AJG_C2D_matched_test
+    %Through editing this test case we can run the program with the intial condition defined in the assignment
+
+    sympref('FloatingPointOutput', true); % Set the solve to bring the numbers to a reasonable decimal
     Ds = user_Ds;
 
-    
+    %convert the zeros and the poles of Ds to Dz through z = e^(s*h)
     zeros_z = exp(h*Ds.z);
     poles_z = exp(h*Ds.p);
 
     m = Ds.num.n; %order of numer
     n = Ds.den.n; %order of denom
 
-    %Determine number of infintie zeros
+    %Determine number of infinity zeros, bring in zeros at z = -1 t
     if n - m > 0 
         zeros_z = [zeros_z, (zeros([1, n - m]) - 1)];
     end
     disp(zeros_z)
     %Determine amount of zeros by causalty
-    if (user_causal == "strict" && n - m > 0)
+    if (user_causal == "strict" && n - m > 0) %If the system is required to be strictly causal then make the order or the numerator one less than the order of the denom
         for i = 0: n - m - 1
-            zeros_z(end - i) = []; %loops until there are enough z zeros removed such that the system is strictly proper
+            zeros_z(end - i) = []; %loops until there are enough z zeros removed such that the system is strictly causal
         end
     end
     
     %Determine Gain
+    %Solve for the gain of Dz by plugging in z = e^(omega*h) and equating it to Ds at s = 0
     z = exp(omega*h);
-    disp(z)
-    disp("zeros")
-    disp(zeros_z)
-    disp("poles")
-    disp(poles_z)
-    disp("evaluate zeros poly")
-    disp(RR_evaluate(RR_poly(zeros_z, 1), z))
-    disp("make poles poly")
-    disp(RR_poly(poles_z, 1))
-    disp("evaluate poles poly at z")
-    disp(RR_evaluate(RR_poly(poles_z, 1), z))
-    disp("evaluate zero poly at z")
-    disp(RR_evaluate(RR_tf(zeros_z, poles_z), z))
-    disp(" zeros / poles")
-    disp((RR_evaluate(RR_poly(zeros_z, 1), z)/ RR_evaluate(RR_poly(poles_z, 1), z)))
-    disp("final")
-    disp(Ds.K / (RR_evaluate(RR_poly(zeros_z, 1), z)/ RR_evaluate(RR_poly(poles_z, 1), z)));
-
     K_z = Ds.K / (RR_evaluate(RR_poly(zeros_z, 1), z)/ RR_evaluate(RR_poly(poles_z, 1), z));
+
+    %Build Dz
     Dz = RR_tf(zeros_z, poles_z, K_z);
 end
 
-%THings to add
-% User defined casualty - If stricly casual then one infinite zero becomes inf
-% user defined omega
-% user defined Ds
-% If we want ds at a frequency it changes our gain
+%Did I set the system to right right gain?  maybe the gain changes when omega isnt 0
--- MAE144/AJG_C2D_matched_test.m
+++ MAE144/AJG_C2D_matched_test.m
@@ -1,7 +1,9 @@
+%AJG_C2D_matched_test 
+% This program intializes and calls the function "AJG_C2D_matched" 
+% The variables are changeable and by commenting out or deleting them we can change the input
 
-syms z p h omega
 
-
+syms z p h omega; % These variables will start as symbolics unless otherwise defined later in the code
 
 h = 0.01;
 z = 5;
@@ -12,7 +14,9 @@ omega = 50;
 
 Ds = RR_tf([1 z],[1 p 0], 1); %D(s)=(s+z1)/[s*(s+p1)]
 
-[Dz] = AJG_C2D_matched(Ds, h, causal, omega)
+[Dz] = AJG_C2D_matched(Ds, h, causal, omega) %call the function
 
-a_Ds = tf([1 z],[1 p 0]);
-[actualDz] = c2d(a_Ds, h, 'matched')
+% If we would like to check our answer for Dz against the MATLAB version, we can run this code
+% Beware that all variables must be defined as numbers to run this code 
+%a_Ds = tf([1 z],[1 p 0]);
+%[actualDz] = c2d(a_Ds, h, 'matched')
