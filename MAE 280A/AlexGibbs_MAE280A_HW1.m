30be8c1340eb3f976b86e731cb49109436bdac91
Alex G <146039097+axgib@users.noreply.github.com>
2023-10-29 16:24:35 -0700

    Adding 283 hw 2
    Need to add hw 2 to the repo

--- MAE 280A/AlexGibbs_MAE280A_HW1.m
+++ MAE 280A/AlexGibbs_MAE280A_HW1.m
@@ -2,13 +2,13 @@
 
 A = [ 1 1 1 0; 1 2 2 1; 2 5 5 3];
 
-A = rref(A); %part A: range space spans {[1 0 0], [ 0 1 0]}
+A = rref(A) %part A: range space spans {[1 0 0], [ 0 1 0]}
 
 RAt = A.';%part B
 
 
 
-%% Exercise 8
+%% Exercise 9
 % clear; clc;
 
 L = [1 2; 7 8; 21 -1];
@@ -21,23 +21,23 @@ X = Z * Z.';
 
 %% P1
 
-eig(P); % >= 0 thus positive semi definite
-eig(X); % > 0 thus positive definite
+eig(P) % >= 0 thus positive semi definite
+eig(X) % > 0 thus positive definite
 %% P2
 
-% A * AT will always produce an nxn matrix 
+% Written in PDF - A * AT will always produce an nxn matrix 
 % and the eigenvalues will always be real because the eigenvectors are
 % linearly dependent. We can prove this by taking the determinant
 %% P3
 
-rankP = rank(P);
-rankX = rank(X);
+rankP = rank(P)
+rankX = rank(X)
 
-eigP = eig(P);
-eigX = eig(X);
+eigP = eig(P)
+eigX = eig(X)
 
-svdP = svd(P);
-svdX = svd(X);
+svdP = svd(P)
+svdX = svd(X)
 %% P4
 
 A = [1 2 3; 2 4 5; 3 5 6];
---
+++ MAE 283A/AlexGibbs_MAE283_HW2_code.m
@@ -0,0 +1,82 @@
+%HW 2 Problem 2.2
+
+clear; clc; close all;
+
+a = 0.9;
+N = 1000;
+e = randn(1, N);
+
+omega = linspace(0, pi, N/2);
+
+H = abs(1 + a.*exp(-1*i*omega));
+spectrum = H.^2 * 1; %heuristic for spectrum white noise
+loglog(omega, spectrum)
+hold on;
+
+
+W = [e(1)];
+for count = 2:length(e)
+    W(count) = e(count) + a * e(count - 1);
+end
+
+Re = xcorr(W);
+omega_adj = [N:2*N-1 2*N-1 1:N-1];
+spectrum_est = fft(Re(omega_adj))/N;
+
+
+
+loglog(omega, real(spectrum_est(1:2:N)))
+
+legend('\Phi', '\Phi_est');
+xlabel("log()")
+ylabel("log(\omega)")
+
+
+
+
+%% Problem 3
+
+clear; clc; 
+
+data = load("mass_spring_damper.mat");
+deltaT = 0.1; %s
+u = data.u;
+y = data.y;
+t = data.t;
+%      b1*F(k-1)  b2*F(k)  a1*x(k-1)   a2*x(k-2)
+PHI = [u(2:end-1) u(3:end) -y(2:end-1) -y(1:end-2)];
+
+Y = y(3:end);
+theta = PHI\Y
+
+
+myY = PHI*theta;
+plot(t, y, "-k")
+hold on
+plot(t(1:end-2), myY)
+legend("measured y(k)", "simulated y_sim(k)");
+xlabel("time t")
+ylabel("Distance - x (m)")
+
+b1 = theta(1);
+b2 = theta(2);
+a1 = theta(3);
+a2 = theta(4);
+
+
+poles_z = roots([1 a1 a2]);
+poles_s = log(poles_z)/deltaT;
+
+gain = (b1 + b2)/(1 + a1 + a2);%when s = 0
+myK = 1/gain; %N/m
+
+syms d m
+r = roots([m d myK]);
+sol = solve(r == poles_s, m, d);
+
+k = myK
+m = double(sol.m) %kg
+d = double(sol.d) %Ns/m
+
+Go = tf( 1, [m, d, k])
+lsim(Go, u, t)
